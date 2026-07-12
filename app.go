f12050d8c9a5652e4126871d4655380fded3e65a
苦瓜 <1348078212@qq.com>
2026-07-12 19:20:20 +0800

    因子回复性能名称更正

--- app.go
+++ app.go
@@ -24,7 +24,7 @@ const (
 	steamAppID  = "881020"
 	gameExeName = "granblue_fantasy_relink.exe"
 	gameFolder  = "Granblue Fantasy Relink"
-	appVersion  = "v1.7.5"
+	appVersion  = "v1.7.6"
 	repoOwner   = "BitterG"
 	repoName    = "GBFR-PE-Patch-Tool"
 )
